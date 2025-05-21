da2166735c44706bce9f49f33244e67853dcbdc1
megaVE <viniherofactory@gmail.com>
2025-05-20 23:31:30 -0300

    Favicons, Logo and Metatags Added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,23 @@
 import githubIcon from "../assets/icons/github-icon.svg";
+import logoIcon from "../assets/logo.png";
 
 export function Header() {
 	return (
-		<header className="bg-black text-gray-100 p-8 antialiased">
-			<div className="border-t-2 border-b-2 border-gray-100 py-4 md:pl-8 text-center md:text-left">
-				<h1 className="text-3xl font-bold upper">Certificate Gallery</h1>
-				<a href="https://github.com/megaVE" className="text-sm">
-					por
-					<img
-						className="inline-block white-filter px-1"
-						src={githubIcon}
-						alt=""
-					/>
-					Vinícius Eduardo
-				</a>
+		<header className="bg-black text-gray-100 p-8">
+			<div className="flex items-center gap-4 border-t-2 border-b-2 border-gray-100 py-4 md:pl-8 text-center md:text-left">
+				<img src={logoIcon} alt="Logo" className="inline-block size-16" />
+				<div>
+					<h1 className="text-3xl font-bold upper">Certificate Gallery</h1>
+					<a href="https://github.com/megaVE" className="text-sm">
+						por
+						<img
+							className="inline-block white-filter px-1"
+							src={githubIcon}
+							alt=""
+						/>
+						Vinícius Eduardo
+					</a>
+				</div>
 			</div>
 		</header>
 	);
